25e48a250bdb47b88cb81079897f680e7d4adca6
MaelleT <maelle.taurand@ac-nantes.fr>
2017-09-23 16:31:55 +0200

    Architecture MVC : mise en place du controlleur

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var indexController = require('../controllers/indexController');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Application de covoiturage' });
-});
+router.get('/',indexController.index);
 
 module.exports = router;
