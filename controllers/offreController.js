a4ae6dd45282ad20e51d564d587b6bc10e3f11b4
MaelleT <maelle.taurand@ac-nantes.fr>
2017-09-24 17:33:06 +0200

    Gestion des offres : définition des routes

--- controllers/offreController.js
+++ controllers/offreController.js
@@ -1,3 +1,15 @@
+
+// Display list of all Offres
 exports.offre_list = function(req, res, next) {
     res.send('NOT IMPLEMENTED: Offres list');
+};
+
+// Display detail page for a specific Offre
+exports.offre_detail = function(req, res, next) {
+    res.send('NOT IMPLEMENTED: Detail Offre id : ' + req.params.id);
+};
+
+// Handle Offre create on POST
+exports.offre_ajout_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Offre create POST');
 };
--- routes/offres.js
+++ routes/offres.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 var offreController = require('../controllers/offreController');
 
-/* GET home page. */
+//GET all Offres
 router.get('/',offreController.offre_list);
+
+//GET specific offre
+router.get('/:id',offreController.offre_detail);
+
+//POST one Offre
+router.post('/',offreController.offre_ajout_post);
+
 
 module.exports = router;
