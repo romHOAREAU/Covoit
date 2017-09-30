669e0fbb5e7a395b2101b5a7ee43cc35402261be
MaelleT <maelle.taurand@ac-nantes.fr>
2017-09-30 20:48:42 +0200

    Lieu : gestion d'une liste de lieu en tant que propriété static

--- models/lieu.js
+++ models/lieu.js
@@ -3,6 +3,8 @@ class Lieu {
     constructor(id,libelle){
         this._id = id;
         this._libelle = libelle;
+
+        Lieu.addLieu(this);
     }
 
     get libelle(){
@@ -13,5 +15,42 @@ class Lieu {
         return 'Lieu : '+ this._libelle;
     }
 
+
+    // Methods for Static Property LES_LIEUX
+    static addLieu(unLieu){
+        Lieu.LES_LIEUX.push(unLieu);
+    }
+
+    static removeLieu(unLieu){
+        let i = Lieu.LES_LIEUX.indexOf(unLieu);
+        if(i != -1) {
+            Lieu.LES_LIEUX.splice(i, 1);
+        }
+    }
+    static nbLieux(){
+        return Lieu.LES_LIEUX.length;
+    }
+
+    static getLesLieux(){
+        return Lieu.LES_LIEUX;
+    }
+
+    static getLesLieuxJSON(){
+        let l = [];
+        Lieu.LES_LIEUX.forEach(
+            function(unLieu){
+                l.push(JSON.parse(JSON.stringify(unLieu)));
+            }
+	  	);
+        return l;
+    }
+    static chargerLieux(){
+        let unlieu1 = new Lieu(1,"Nantes");
+        let unLieu2 = new Lieu(2,"Orvault");
+    }
 }
+
+Lieu.LES_LIEUX = [];
+
+
 module.exports = Lieu;
--- test/test_lieu.js
+++ test/test_lieu.js
@@ -7,15 +7,37 @@ describe("Lieu",() => {
     describe("create Lieu",() => {
         let unlieu1;
 
-
         before(function () {
             unlieu1 = new Lieu(1,"Nantes");
-
         });
 
         it("Création d'un lieu",() => {
-            assert.equal(unlieu1.libelle, 'Nantes', 'Problème de création');
+            assert.equal(Lieu.nbLieux(), 1, 'Problème de création');
         });
+
+        after(function () {
+            Lieu.removeLieu(unlieu1);
+        });
+
+    });
+    describe("create many Lieux",() => {
+        let unlieu1;
+        let unlieu2;
+
+        before(function () {
+            unlieu1 = new Lieu(1,"Nantes");
+            unlieu2 = new Lieu(2,"Orvault");
+        });
+
+        it("Création d'un lieu",() => {
+            assert.equal(Lieu.nbLieux(), 2, 'Problème de création');
+        });
+
+        after(function () {
+            Lieu.removeLieu(unlieu1);
+            Lieu.removeLieu(unlieu2);
+        });
+
 
 
     });
